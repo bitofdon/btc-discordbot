06e577cb5ea5d7bcd2a1ebda337e7c4842cdc53e
bitofdon <bitdon@protonmail.com>
2019-04-12 10:28:50 -0500

    client command for possible responses when a user enters: '' in Discord

--- my_bot.py
+++ my_bot.py
@@ -3,6 +3,7 @@ import json
 from requests import Request, Session
 import requests
 from discord.ext import commands
+import random
 
 #for tokens/keys
 with open('config.json', 'r') as infile:
@@ -17,5 +18,17 @@ async def on_ready():
     print('Bot is now online and ready')
 
 
+#COMMANDS HERE
+#on $when_moon - generate one of the possible responses - must import random
+@client.command()
+async def when_moon(ctx):
+    possible_responses = [
+        'Hopefully soon',
+        'Never',
+        'Bottom is in',
+        'just HODL'
+    ]
+    #await client.say(random.choice(possible_responses)) - this method is outdated
+    await ctx.send(random.choice(possible_responses))
 
 client.run(bot_token)
